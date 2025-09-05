e9b01a8e399a2ad6563733a2d56cb95b73b3a331
ProfJust <olaf.just@w-hs.de>
2025-09-05 13:02:27 +0200

    turtelgrafik

---
+++ p01/p01_turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+t = turtle.Turtle()
+for i in range(4):
+    t.forward(100)
+    t.right(90)
+
+
+for j in range(19):
+    t.circle(50)
+    t.right(10)   
+
+turtle.done()  # Fenster offen halten
