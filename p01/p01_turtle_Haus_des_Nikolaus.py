f50c501f5f01693393797393472de9241adc856f
ProfJust <olaf.just@w-hs.de>
2025-09-08 12:02:40 +0200

    new

---
+++ p01/p01_turtle_Haus_des_Nikolaus.py
@@ -0,0 +1,25 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.left(90)        # Turtle nach oben drehen
+t.forward(100)
+t.right(45)
+t.forward(71)
+t.right(90)
+#input("weiter?")
+t.forward(71)
+t.right(135)
+t.forward(100)
+#iput("weiter?")
+t.left(135)
+t.forward(141)
+t.right(135)
+t.forward(100)
+t.right(135)
+t.forward(141)
+t.right(135)
+t.forward(100)
+
+
+turtle.done()  # Fenster offen halten
---
+++ p01/turtle_graphics.py
@@ -0,0 +1,8 @@
+# turtle_graphics.py
+import turtle
+
+turtle.forward(100)   # Bewegt die Schildkröte 100 Pixel vorwärts
+turtle.left(90)       # Dreht die Schildkröte um 90 Grad nach links
+turtle.color('red')
+turtle.circle(50)     # Zeichnet einen Kreis mit Radius 50 Pixel
+turtle.done()         # Fenster offen halten
