00622f5efff56e2f9a003ba000fa440385b5f27b
ProfJust <olaf.just@w-hs.de>
2025-09-29 12:19:17 +0200

    yep

--- pyQt/PyQt_sw02_signal_slot_button.py
+++ pyQt/PyQt_sw02_signal_slot_button.py
@@ -1,13 +1,20 @@
-import sys
-# Die Bibliothek PyQt6 wird genutzt, insbesondere die Widget-Elemente wie QMainWindow und QPushButton. 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 #-------------------------------------------------------------------
 # Dieser Code erstellt mit PyQt6 ein einfaches GUI-Fenster mit einem Button, 
 # der beim Anklicken eine Meldung in der Konsole ausgibt.
+# OJ 29.09.2025
 #-------------------------------------------------------------------
-from PyQt6.QtCore import QSize    
 
-class MainWindow(QMainWindow):
+# sys wird verwendet, um Programm-Argumente zu verarbeiten und das Programm später zu beenden
+import sys
+
+# Die Bibliothek PyQt6 wird genutzt, insbesondere die Widget-Elemente wie QMainWindow und QPushButton. 
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+# Ermöglicht das Festlegen der Fenstergröße als QSize-Objekt.
+from PyQt6.QtCore import QSize   
+
+
+class MainWindow(QMainWindow): # Leitet eine benutzerdefinierte Fensterklasse MainWindow von QMainWindow ab.
     def __init__(self):
         super().__init__()
         # Die zentrale Komponente ist ein Fenster, das von QMainWindow erbt. 
@@ -29,10 +36,11 @@ class MainWindow(QMainWindow):
         pybutton.move(50, 50)        
 
     def clickSlot(self):
-        print('Clicked Pyqt button.')
+        # Die Methode clickSlot(self) wird durch das Klick-Ereignis des Buttons ausgelöst 
+        print('Clicked Pyqt button.') # gibt den Text „Clicked Pyqt button.“ auf der Konsole aus.
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    mainWin = MainWindow()
-    mainWin.show()
-    sys.exit( app.exec() )
+    app = QApplication(sys.argv)  # eine Anwendung wird erzeugt
+    mainWin = MainWindow()  # Ein Objekt von MainWindow wird erzeugt 
+    mainWin.show()          # ..und mit mainWin.show() sichtbar gemacht.
+    sys.exit( app.exec() )  # startet die Event-Schleife von Qt und sorgt für sicheres Beenden der Anwendung.
