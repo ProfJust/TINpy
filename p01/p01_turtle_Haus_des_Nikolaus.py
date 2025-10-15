309439f26591dcfe87c47e86419f3afdefe5f272
ProfJust <olaf.just@w-hs.de>
2025-10-15 08:12:38 +0200

    comments changed

--- p01/p01_turtle_Haus_des_Nikolaus.py
+++ p01/p01_turtle_Haus_des_Nikolaus.py
@@ -1,6 +1,6 @@
 import turtle
 
-t = turtle.Turtle()
+t = turtle.Turtle()  # Instanz bilden
 
 t.left(90)        # Turtle nach oben drehen
 t.forward(100)
@@ -8,7 +8,7 @@ t.right(45)
 t.forward(71)
 t.right(90)
 
-##### Hier Code Ergänzen #######
+# #### Hier Code Ergänzen #######
 
 
 turtle.done()  # Fenster offen halten
