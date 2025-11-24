07d80f32d6bef2a1e11e41285ecfffb723fdaf39
ProfJust <olaf.just@w-hs.de>
2025-11-24 12:48:23 +0100

    mit parent

--- py_Vorlesung/sw05_Dateien/sw05_1d_pathlib.py
+++ py_Vorlesung/sw05_Dateien/sw05_1d_pathlib.py
@@ -11,13 +11,20 @@ print(text)
 p = script_dir / "log.txt"
 text = p.read_text()
 print(text)
-p.write_text("Neuer Text im Logfile.\n")    
-
+p.write_text("Neuer Text im Logfile.\n")  
+print("path: ",p)
+print("name: ", p.name) # 
+print("stem: ", p.stem) # 
+print("suffix: ", p.suffix) # '.pdf'
+print("parent: ", p.parent) # '/home/olaf/berichte' 
+print("\n ---------- \n")
 # globale Suche nach Dateien => GREAT !!
 for f in Path(".").glob("config.yaml"):
     print(f)
     if f.exists():
-        text = f.read_text(encoding="utf-8")
-        print(text)
+        text = f.read_text(encoding="utf-8")    
+        print(text)    
     else:
         print(f"Pfad {f} nicht gefunden")
+
+
