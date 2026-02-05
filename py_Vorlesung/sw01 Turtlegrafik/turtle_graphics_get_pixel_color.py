e11afde8290a914173e31945a4e9f95ce297948a
ProfJust <olaf.just@w-hs.de>
2026-02-05 10:25:12 +0100

    yep

--- py_Vorlesung/sw01 Turtlegrafik/turtle_graphics_get_pixel_color.py
+++ py_Vorlesung/sw01 Turtlegrafik/turtle_graphics_get_pixel_color.py
@@ -15,19 +15,21 @@ def get_pixel_color(x, y):
     return "white"
 
 
-turtle = turtle.Turtle()   # Instanziere ein Objekt der Klasse turtle()
-turtle.forward(100)   # Bewegt die Schildkröte 100 Pixel vorwärts
-turtle.left(90)       # Dreht die Schildkröte um 90 Grad nach links
-turtle.color('red')
-turtle.circle(50)     # Zeichnet einen Kreis mit Radius 50 Pixel
+kroete = turtle.Turtle()   # Instanziere ein Objekt der Klasse turtle()
+kroete.color('black')
+kroete.forward(100)   # Bewegt die Schildkröte 100 Pixel vorwärts
+kroete.left(90)       # Dreht die Schildkröte um 90 Grad nach links
+kroete.color('red')
+kroete.circle(50)     # Zeichnet einen Kreis mit Radius 50 Pixel
 
 # Farbe an bestimmten Pixeln abfragen
-turtle.goto(0, 0)  # Gehe zurück zum Ursprung
+kroete.penup()  # Stift anheben, um keine Linien zu zeichnen
+kroete.goto(0, 0)  # Gehe zurück zum Ursprung
 color_at_origin = get_pixel_color(0, 0)
 print(f"Farbe am Ursprung (0,0): {color_at_origin}")
-turtle.goto(100, 0)  # Gehe zu (100,0)
-color_at_100_0 = get_pixel_color(100, 0)        
-print(f"Farbe bei (100,0): {color_at_100_0}")
+kroete.goto(100, 0)  # Gehe zu (100,0)  x, y
+color_at_100_0 = get_pixel_color(100, 10)        
+print(f"Farbe bei (100, 10): {color_at_100_0}")
 
-turtle.done()         # Fenster offen halten
-input("Drücken Sie die Eingabetaste, um das Programm zu beenden...")
+turtle.done()  # Beendet die Turtle-Grafik und hält das Fenster offen
+# input("Drücken Sie die Eingabetaste, um das Programm zu beenden...")
